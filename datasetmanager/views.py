1b8e649fa5917f1bea96d3a5440e7b1590986f42
Adityasingh51 <adityapratapsinghom@gmail.com>
2025-04-25 16:47:17 +0530

    commit

--- datasetmanager/views.py
+++ datasetmanager/views.py
@@ -10,6 +10,14 @@ def browse(request):
     }
     return render(request, "browse.html", context)
 
+def browse(request):
+    query = request.GET.get('q')  # Get the search query from the URL
+    if query:
+        datasetList = Datasetmanager.objects.filter(name__icontains=query)  # Filter datasets by name
+    else:
+        datasetList = Datasetmanager.objects.all()  # Show all datasets if no query
+    return render(request, "browse.html", {"datasetList": datasetList})
+
 def datasetDetails(request, dataset_id):
     dataset = get_object_or_404(Datasetmanager, id=dataset_id)
     context = {
