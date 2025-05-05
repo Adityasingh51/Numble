d4f98e78179fec34d714de15ed672b8e59f2dc1f
Adityasingh51 <adityapratapsinghom@gmail.com>
2025-05-05 15:23:04 +0530

    commit

--- datasetmanager/models.py
+++ datasetmanager/models.py
@@ -24,3 +24,12 @@ class Review(models.Model):
 
     def __str__(self):
         return f"Review for {self.dataset.name} - {self.rating} Stars"
+
+
+class Feedback(models.Model):
+    dataset = models.ForeignKey(Datasetmanager, on_delete=models.CASCADE, related_name='feedback')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Feedback for {self.dataset.name}"
