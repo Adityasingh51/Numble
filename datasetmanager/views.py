9c61c72df5fc4232cb5e761a179441bd59cb32d5
Adityasingh51 <adityapratapsinghom@gmail.com>
2025-05-15 19:29:30 +0530

    commit

--- datasetmanager/views.py
+++ datasetmanager/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Datasetmanager, Review  # Import the Review model
-from django.contrib import messages  # For user feedback
+from .models import Datasetmanager, Review 
+from django.contrib import messages 
 from django.contrib.auth.decorators import login_required
 
-# For Search Functionality
 def browse(request):
-    query = request.GET.get('q')  # Get the search query from the URL
+    query = request.GET.get('q') 
     if query:
-        datasetList = Datasetmanager.objects.filter(name__icontains=query)  # Filter datasets by name
+        datasetList = Datasetmanager.objects.filter(name__icontains=query)  
     else:
-        datasetList = Datasetmanager.objects.all()  # Show all datasets if no query
+        datasetList = Datasetmanager.objects.all() 
     return render(request, "browse.html", {"datasetList": datasetList})
 
 def datasetDetails(request, dataset_id):
@@ -25,20 +24,18 @@ def submit_review(request, dataset_id):
         rating = request.POST.get("rating")
         review_text = request.POST.get("review")
 
-        # Save the review
         Review.objects.create(
             dataset=dataset,
             rating=rating,
             text=review_text,
         )
 
-        # Add a success message
         messages.success(request, "Your review has been submitted successfully!")
 
-        # Redirect back to the dataset details page
+        
         return redirect("datasetDetails", dataset_id=dataset_id)
     else:
-        # If the request is not POST, redirect to the dataset details page
+        
         return redirect("datasetDetails", dataset_id=dataset_id)
 
 @login_required
